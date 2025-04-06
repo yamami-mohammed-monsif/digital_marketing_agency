9d243ac90ec8767c4e4c3fc91214e60cfb9e7beb
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-04-06 02:03:11 +0100

    feat: implement about section with responsive layout and text reveal

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const page = () => {
   return (
     <main className="min-h-screen">
       <Hero />
+      <About />
     </main>
   );
 };
---
+++ components/About.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Section from "./UI/Section";
+import Reveal from "./UI/Reveal";
+
+const About: React.FC = () => {
+  return (
+    <Section className="md:grid grid-cols-2">
+      <Reveal>
+        <h2>About Us!</h2>
+      </Reveal>
+      <div>
+        <Reveal>
+          <p className="mb-10 md:mb-6">
+            Our services help you create digital products and solve your
+            problems objectively, strategy, technology and analysis. Our service
+            also has a high appeal because it has a beautiful color combination
+            and a minimalist concept.
+          </p>
+        </Reveal>
+
+        <div className="md:hidden flex flex-col items-center gap-6 mb-6">
+          <Reveal>
+            <div>
+              <span className="block font-bold text-[64px] leading-[100%] mb-4">
+                300+
+              </span>
+              <span className="text-[16px] font-medium">Project Completed</span>
+            </div>
+          </Reveal>
+          <Reveal>
+            <div>
+              <span className="block font-bold text-[64px] leading-[100%] mb-4">
+                25+
+              </span>
+              <span className="text-[16px] font-medium">
+                Professional Teams
+              </span>
+            </div>
+          </Reveal>
+          <Reveal>
+            <div>
+              <span className="block font-bold text-[64px] leading-[100%] mb-4">
+                300+
+              </span>
+              <span className="text-[16px] font-medium">Project Completed</span>
+            </div>
+          </Reveal>
+          <Reveal>
+            <div>
+              <span className="block font-bold text-[64px] leading-[100%] mb-4">
+                25+
+              </span>
+              <span className="text-[16px] font-medium">
+                Professional Teams
+              </span>
+            </div>
+          </Reveal>
+        </div>
+        <div>
+          <Reveal>
+            <h3>What We do?</h3>
+          </Reveal>
+          <Reveal>
+            <p>
+              We provide comprehensive marketing solutions that help our clients
+              reach their maximum potential. Our team of experienced
+              professionals understands the ever-changing landscape of marketing
+              and is able to create custom strategies for each client. From
+              digital marketing to traditional media and everything in between,
+              we have the expertise to put together an effective, successful
+              marketing campaign. We also offer solutions in the areas of public
+              relations, branding, search engine optimization, and more. We
+              strive to deliver measurable, tangible results that help our
+              clients reach their goals.
+            </p>
+          </Reveal>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default About;
